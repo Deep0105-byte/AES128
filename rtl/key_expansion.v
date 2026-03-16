d77159215b3aa8ed64f9e02867ab78de7b821f62
Deepak <deepakkumar484844@gmail.com>
2026-03-16 16:49:37 +0530

    Update in shiftrows module

--- rtl/key_expansion.v
+++ rtl/key_expansion.v
@@ -1,62 +1,54 @@
 module key_expansion(
 input  [127:0] key,
-input  [3:0] round,
+input  [3:0]   round,
 output [127:0] round_key
 );
 
-reg [31:0] w [0:43];
+reg [31:0] w[0:43];
 integer i;
 
 function [31:0] rcon;
 input [3:0] r;
 begin
 case(r)
-1: rcon = 32'h01000000;
-2: rcon = 32'h02000000;
-3: rcon = 32'h04000000;
-4: rcon = 32'h08000000;
-5: rcon = 32'h10000000;
-6: rcon = 32'h20000000;
-7: rcon = 32'h40000000;
-8: rcon = 32'h80000000;
-9: rcon = 32'h1b000000;
+1:  rcon = 32'h01000000;
+2:  rcon = 32'h02000000;
+3:  rcon = 32'h04000000;
+4:  rcon = 32'h08000000;
+5:  rcon = 32'h10000000;
+6:  rcon = 32'h20000000;
+7:  rcon = 32'h40000000;
+8:  rcon = 32'h80000000;
+9:  rcon = 32'h1b000000;
 10: rcon = 32'h36000000;
 default: rcon = 32'h00000000;
 endcase
 end
 endfunction
 
-function [31:0] rotword;
-input [31:0] x;
+function [7:0] sbox;
+input [7:0] a;
 begin
-rotword = {x[23:0], x[31:24]};
-end
-endfunction
-
-function [7:0] sbox_lookup;
-input [7:0] x;
-begin
-case(x)
-8'h00: sbox_lookup=8'h63; 8'h01: sbox_lookup=8'h7c;
-8'h02: sbox_lookup=8'h77; 8'h03: sbox_lookup=8'h7b;
-8'h04: sbox_lookup=8'hf2; 8'h05: sbox_lookup=8'h6b;
-8'h06: sbox_lookup=8'h6f; 8'h07: sbox_lookup=8'hc5;
-8'h08: sbox_lookup=8'h30; 8'h09: sbox_lookup=8'h01;
-8'h0a: sbox_lookup=8'h67; 8'h0b: sbox_lookup=8'h2b;
-default: sbox_lookup=8'h00;
+case(a)
+8'h00: sbox=8'h63; 8'h01: sbox=8'h7c; 8'h02: sbox=8'h77; 8'h03: sbox=8'h7b;
+8'h04: sbox=8'hf2; 8'h05: sbox=8'h6b; 8'h06: sbox=8'h6f; 8'h07: sbox=8'hc5;
+8'h08: sbox=8'h30; 8'h09: sbox=8'h01; 8'h0a: sbox=8'h67; 8'h0b: sbox=8'h2b;
+default: sbox=8'h00;
 endcase
 end
 endfunction
 
 function [31:0] subword;
-input [31:0] x;
+input [31:0] w;
 begin
-subword = {
-sbox_lookup(x[31:24]),
-sbox_lookup(x[23:16]),
-sbox_lookup(x[15:8]),
-sbox_lookup(x[7:0])
-};
+subword = {sbox(w[31:24]),sbox(w[23:16]),sbox(w[15:8]),sbox(w[7:0])};
+end
+endfunction
+
+function [31:0] rotword;
+input [31:0] w;
+begin
+rotword = {w[23:0],w[31:24]};
 end
 endfunction
 
@@ -69,10 +61,10 @@ w[3] = key[31:0];
 
 for(i=4;i<44;i=i+1)
 begin
-if(i%4==0)
-w[i] = w[i-4] ^ subword(rotword(w[i-1])) ^ rcon(i/4);
-else
-w[i] = w[i-4] ^ w[i-1];
+    if(i%4==0)
+        w[i] = w[i-4] ^ subword(rotword(w[i-1])) ^ rcon(i/4);
+    else
+        w[i] = w[i-4] ^ w[i-1];
 end
 
 end
@@ -88,6 +80,6 @@ assign round_key =
 (round==7)  ? {w[28],w[29],w[30],w[31]} :
 (round==8)  ? {w[32],w[33],w[34],w[35]} :
 (round==9)  ? {w[36],w[37],w[38],w[39]} :
-{w[40],w[41],w[42],w[43]};
+              {w[40],w[41],w[42],w[43]};
 
 endmodule
